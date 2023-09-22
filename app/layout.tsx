6f04457c2b4a79d07bf98186a28e5431041d52ed
dondycles <johnrod.dondoyano@gmail.com>
2023-09-22 16:27:56 +0800

    modified:   app/layout.tsx 	modified:   package.json 	modified:   pnpm-lock.yaml

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import NextUIProvider from "@/components/provider/nextUi";
 import "./globals.css";
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 export const metadata: Metadata = {
   title: "Moneytor | The Money Monitorer",
   description: "Monitor your money with Moneytor.",
@@ -13,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
       <html lang="en">
         <body>
           <NextUIProvider>{children}</NextUIProvider>
