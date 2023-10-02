6bc5fb427425e3d276557cf16c0c59ff166a9cea
dondycles <johnrod.dondoyano@gmail.com>
2023-10-03 00:56:33 +0800

    modified:   app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://moneytor-ph.vercel.app/"),
   title: "Moneytor",
   description: "Manage your finances with ease using Moneytor.",
   twitter: {
@@ -21,7 +22,7 @@ export const metadata: Metadata = {
     description: "Manage your finances with ease using Moneytor.",
     type: "website",
     siteName: "Moneytor",
-    url: "moneytor-nextui.vercel.app/",
+    url: "https://moneytor-ph.vercel.app/",
     images: [
       {
         url: "/summary.png",
