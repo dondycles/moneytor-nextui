b03e2385ccc535d39a8db833a7b8d090ca95cb5b
dondycles <johnrod.dondoyano@gmail.com>
2023-10-06 00:06:19 +0800

    modified:   components/provider/nextUi.tsx 	modified:   next.config.js 	modified:   package.json 	modified:   pnpm-lock.yaml 	deleted:    worker.ts

--- components/provider/nextUi.tsx
+++ components/provider/nextUi.tsx
@@ -3,7 +3,6 @@ import { useTheme } from "@/store";
 import { NextUIProvider as Provider, Spinner } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import { Montserrat } from "next/font/google";
-import { Workbox } from "workbox-window";
 
 export const montserrat = Montserrat({
   subsets: ["latin"],
@@ -20,16 +19,6 @@ export default function NextUIProvider({
   const theme = useTheme();
   useEffect(() => {
     setHydrated(true);
-    if (
-      !("serviceWorker" in navigator) ||
-      process.env.NODE_ENV !== "production"
-    ) {
-      console.warn("Pwa support is disabled");
-      return;
-    }
-
-    const wb = new Workbox("sw.js", { scope: "/" });
-    wb.register();
   }, []);
   if (hydrated)
     return (
--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-const withWorkbox = require("next-with-workbox");
-module.exports = withWorkbox({
-  workbox: {
-    swSrc: "worker.ts",
-  },
-  nextConfig,
-});
+const withPWA = require("next-pwa");
+const nextConfig = {
+  ...withPWA({
+    dest: "public",
+    register: true,
+    skipWating: true,
+  }),
+};
+
+module.exports = nextConfig;
